b55ef2a00a1f6c05e47b197b8745ee6ff684e6ee
Jesus Ayala <jesusayala1996cult@gmail.com>
2024-02-27 19:02:11 -0500

    Signals y visualizar tareas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  tasks = signal(['Instalar Angular CLI', 'Crear el Proyecto', 'Crear Componentes'])
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
@@ -10,8 +10,8 @@ import { Component } from '@angular/core';
 })
 export class LabsComponent {
   welcome = 'Hola Angular';
-  tasks = ['Instalar Angular CLI', 'Crear el Proyecto', 'Crear Componentes'];
-  name = 'Nicolas';
+  tasks = signal(['Instalar Angular CLI', 'Crear el Proyecto', 'Crear Componentes']);
+  name = signal('Nicolas');
   disabled = true;
   img = 'https://w3schools.com/howto/img_avatar.png';
 
@@ -20,4 +20,17 @@ export class LabsComponent {
     age: 18,
     avatar: 'https://w3schools.com/howto/img_avatar.png',
   };
+
+  clickHandler(){
+    alert("Hola")
+  };
+  changeHandler(event:Event){
+    const input = event.target as HTMLInputElement
+    const newValue = input.value
+    this.name.set(newValue)
+  }
+  keydownHandler(event:KeyboardEvent){
+    const input = event.target as HTMLInputElement
+    console.log(input.value)
+  }
 }
